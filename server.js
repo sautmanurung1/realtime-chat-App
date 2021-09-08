beb33471d18ea02e96e42cce679ffaee5c85c72d
sautmanurung1 <smanurung360@gmail.com>
2021-09-08 16:49:17 +0700

    create vercel.json

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path')
 const app = express()
 const http = require('http').createServer(app)
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 3000
 http.listen(PORT, ()=>{
     console.log('Server is running on port', PORT);
